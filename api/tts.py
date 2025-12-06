79e7ca2c033b6503ac437e5e85d593b02a256908
javacore-insight <javacore.ai@gmail.com>
2025-12-06 21:25:03 +0700

    Update V2 tts.py

--- api/tts.py
+++ api/tts.py
@@ -3,12 +3,16 @@ from io import BytesIO
 
 def handler(request):
     text = request.args.get("text", "")
+
     if not text:
-        return {"status": 400, "body": "Error: missing text"}
+        return {
+            "status": 400,
+            "body": "Text is required"
+        }
 
     buf = BytesIO()
     gTTS(text=text, lang='id').write_to_fp(buf)
-    mp3_data = buf.getvalue()
+    mp3_bytes = buf.getvalue()
 
     return {
         "status": 200,
@@ -16,5 +20,5 @@ def handler(request):
             "Content-Type": "audio/mpeg",
             "Content-Disposition": "attachment; filename=tts.mp3"
         },
-        "body": mp3_data
+        "body": mp3_bytes
     }
